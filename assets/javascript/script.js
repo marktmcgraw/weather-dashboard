2e5cf85224c414adfb921b7311db933501f185f7
Cotton Top <mtmcgr@gmail.com>
2019-12-03 08:16:23 -0600

    fixed API call function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,10 @@
-const userInput = document.getElementById("user-input").value;
-const APIKey = "&APPID=ef2e34e235b9c5156c08f25db07cfa7a";
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + APIKey;
 
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ef2e34e235b9c5156c08f25db07cfa7a
-
-axios.get(queryURL)
-.then(function(response){
-    console.log(queryURL);
-    console.log(response);
-    document.getElementById("search-results").innerHTML = JSON.stringify(response);
-
- });
-
-
-
+// const queryURL =
+// const APIKey = 
 
 $("#search-button").on("click", function (event) {
     event.preventDefault();
+
     const value = $("#user-input").val();
     // Get array of values from local storage; if array doesn't exist, create a blank array.
     const history = JSON.parse(localStorage.getItem("history")) || [];
@@ -27,6 +15,26 @@ $("#search-button").on("click", function (event) {
 
     makeBootstrapCard(value);
     // Append that bootstrap card to the page
+
+    const userInput = document.getElementById("user-input").value;
+    console.log(userInput)
+    const APIKey = "&appid=ef2e34e235b9c5156c08f25db07cfa7a"
+    const queryURL = "https://api.openweathermap.org/data/2.5/find?q=" + userInput + "&units=imperial" + APIKey;
+
+
+    axios.get(queryURL)
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+            document.getElementById("current-temp").innerHTML = "Temperature (F): " + response.data.list[0].main.temp;
+            document.getElementById("current-humidity").innerHTML = "Humidity: " + response.data.list[0].main.humidity;
+            document.getElementById("windspeed").innerHTML = "Wind Speed: " + response.data.list[0].wind.speed;
+           // document.getElementById("uv-index").innerHTML = "UV Index: " + response.data.list[0].main.temp;
+
+
+
+
+        });
 });
 
 // Make new bootstrap card with user input
